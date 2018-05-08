2366ad5c4adf117db8ae44c250e454f7409a4acd
wojtek <wojtek@tappest.com>
2018-05-08 21:53:13 +0200

    create quote project

---
+++ Vue/project2/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+    
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                quotes: [
+                    'Just something to display on screen'
+                ],
+                maxQuotes: 10
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
