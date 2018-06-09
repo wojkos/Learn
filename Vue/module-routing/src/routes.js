b217f1fe32fac22744006f3ab69640d64cf9e2c9
wojtek <wojtek@tappest.com>
2018-06-09 19:31:41 +0200

    lazy loading

--- Vue/module-routing/src/routes.js
+++ Vue/module-routing/src/routes.js
@@ -1,9 +1,32 @@
 import Home from './Home.vue';
-import User from './components/user/User.vue';
-import UserStart from './components/user/UserStart.vue';
-import UserDetail from './components/user/UserDetail.vue';
-import UserEdit from './components/user/UserEdit.vue';
 import Header from './Header.vue'
+
+const User = resolve => {
+    require.ensure(['./components/user/User.vue'], () => {
+        resolve(require('./components/user/User.vue'));
+    });
+};
+const UserStart = resolve => {
+    require.ensure(['./components/user/UserStart.vue'], () => {
+        resolve(require('./components/user/UserStart.vue'));
+    });
+};
+const UserDetail = resolve => {
+    require.ensure(['./components/user/UserDetail.vue'], () => {
+        resolve(require('./components/user/UserDetail.vue'));
+    });
+};
+const UserEdit = resolve => {
+    require.ensure(['./components/user/UserEdit.vue'], () => {
+        resolve(require('./components/user/UserEdit.vue'));
+    });
+};
+// can add group-name to download each grouped components in one time
+// const UserEdit = resolve => {
+//     require.ensure(['./components/user/UserEdit.vue'], () => {
+//         resolve(require('./components/user/UserEdit.vue'));
+//     }, 'group-name');
+// };
 
 export const routes = [{
         path: '',
