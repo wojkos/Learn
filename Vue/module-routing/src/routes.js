2e701663d2b18fc5d5d7a59903342b0f85d74f53
wojtek <wojtek@tappest.com>
2018-06-09 18:19:25 +0200

    rescue for unknown path

--- Vue/module-routing/src/routes.js
+++ Vue/module-routing/src/routes.js
@@ -24,5 +24,7 @@ export const routes = [{
             { path: ':id', component: UserDetail },
             { path: ':id/edit', component: UserEdit, name: 'userEdit' }
         ]
-    }
+    },
+    { path: 'redirect-me', redirect: { name: 'home' } },
+    { path: '*', redirect: { name: 'home' } }
 ];
